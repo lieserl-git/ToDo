e73f520849b4b6f1f52c10af36b5ef68c3c673f1
lieserl-git <camilavco1110@protonmail.com>
2026-05-28 17:07:34 -0300

    Update: main

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import flet as ft
 
 
-def main(page: ft.Page):
-    def add_clicked(e):
-        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        view.update()
+@ft.control
+class TodoApp(ft.Column):
+    def init(self):
+        self.new_task = ft.TextField(hint_text="What do you want to do today?", expand=True)
+        self.tasks_view = ft.Column()
+        self.width = 600
+        self.controls = [ft.Row(controls=[self.new_task,ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=self.add_clicked, foreground_color = ft.Colors.BLACK, bgcolor = ft.Colors.WHITE),],),self.tasks_view,]
 
-    new_task = ft.TextField(hint_text="What do you want to do today?", expand=True)
-    tasks_view = ft.Column()
-    view = ft.Column(
-        width=600,
-        controls=[
-            ft.Row(
-                controls=[
-                    new_task,
-                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
-                ],
-            ), tasks_view,
-        ],
-    )
+    def add_clicked(self, e):
+        self.tasks_view.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
+
+def main(page: ft.Page):
+    page.title = "To-Do App"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.window.width = 500
+    page.window.height = 500
+    page.window.resizable = False
 
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
 
-
+    todo = TodoApp()
+    page.add(todo)
+    
 ft.run(main)
