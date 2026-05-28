c03d49e09ee014517498a64e6ab0f65b359990aa
lieserl-git <camilavco1110@protonmail.com>
2026-05-28 16:45:08 -0300

    Update: main

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import flet
-from flet import Checkbox, FloatingActionButton, Icons, Page, TextField
+import flet as ft
 
 
-def main(page: Page):
+def main(page: ft.Page):
     def add_clicked(e):
-        page.add(Checkbox(label=new_task.value))
+        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
         new_task.value = ""
-        page.update()
+        view.update()
 
-    new_task = TextField(hint_text="What do you want to do today?")
+    new_task = ft.TextField(hint_text="What do you want to do today?", expand=True)
+    tasks_view = ft.Column()
+    view = ft.Column(
+        width=600,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task,
+                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
+                ],
+            ), tasks_view,
+        ],
+    )
 
-    page.add(new_task, FloatingActionButton(icon=Icons.ADD, on_click=add_clicked))
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
 
 
-flet.run(main)
+ft.run(main)
